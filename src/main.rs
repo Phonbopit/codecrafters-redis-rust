6f4799c0c8b55a06b07a4d0cad99c1a10faef5a1
Chai Phonbopit <phonbopit@gmail.com>
2023-09-11 19:49:44 +0700

    pass 4nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,44 @@
-// Uncomment this block to pass the first stage
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use anyhow::Result;
+use bytes::BytesMut;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
+async fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let mut buff = BytesMut::with_capacity(512);
+
+    loop {
+        // wait for client to send us a message
+        let bytes_read = stream.read_buf(&mut buff).await?;
+        if bytes_read == 0 {
+            println!("client closed connection!");
+            break;
+        }
+
+        stream.write("+PONG\r\n".as_bytes()).await?;
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let incoming = listener.accept().await;
+
+        match incoming {
+            Ok((stream, _)) => {
                 println!("accepted new connection.");
-                let mut buff = [0; 512];
 
-                loop {
-                    // wait for client to send us a message
-                    let bytes_read = stream.read(&mut buff).unwrap();
-                    if bytes_read == 0 {
-                        println!("client closed connection!");
-                        break;
-                    }
-
-                    stream.write("+PONG\r\n".as_bytes()).unwrap();
-                }
+                tokio::spawn(async move {
+                    handle_connection(stream).await.unwrap();
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
