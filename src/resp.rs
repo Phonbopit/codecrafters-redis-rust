1be2f7f8bac8962f2007c24bd9912b9092871550
Chai Phonbopit <Phonbopit@gmail.com>
2023-09-23 21:14:17 +0700

    fix: add Null case

--- src/resp.rs
+++ src/resp.rs
@@ -41,6 +41,7 @@ impl Value {
 
     pub fn encode(self) -> String {
         match &self {
+            Value::Null => "$-1\r\n".to_string(),
             Value::SimpleString(s) => format!("+{}\r\n", s.as_str()),
             Value::Error(msg) => format!("-{}\r\n", msg.as_str()),
             Value::BulkString(s) => format!("${}\r\n{}\r\n", s.chars().count(), s),
